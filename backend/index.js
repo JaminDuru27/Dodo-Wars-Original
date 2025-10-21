92905a1977f4c1560496019b4faff5b193d306c5
Jamin Duru <jaminduru27@example.com>
2025-10-21 02:14:08 -0700

    newest commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log(`kk`)
+const express = require('express')
+const app = express(app)
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors:{
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+const port = 5000
